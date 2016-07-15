eb1a79d886035df2367df158ddc124879e0c33b3
windform <867185661@qq.com>
2016-07-15 17:35:40 +0800

    20160715:web change1

--- web/app.js
+++ web/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
-var MongoStore = require('connect-mongo');
-var settings = require('../settings');
+//var MongoStore = require('connect-mongo');
+//var settings = require('../settings');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,15 +27,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.session({
+/*app.use(express.session({
   secret:settings.cookieSecret,
   store:new MongoStore({
     db:settings.db
   })
-}))
+}))*/
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: '首页' });
+})
+.get('/reg',function(req, res, next){
+  res.render('reg', { title: '用户注册' });
+})
+.post('/reg',function(req, res, next){
+  alert('hello')
+})
 
 module.exports = router;
